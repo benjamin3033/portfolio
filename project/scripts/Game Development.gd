f97b4af00379a054e10235e234966bd6db3934da
Benji <63519523+benjamin3033@users.noreply.github.com>
2024-07-14 09:47:28 +1200

    changes

---
+++ project/scripts/Game Development.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+@export var title_label : Label
+@export var back_button : Button
+@export var main_menu_control : Control
+
+@export var title_end_position : Vector2
+@export var back_button_end_position : Vector2
+
+func show_screen():
+	title_label.text = "Game Development"
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(title_label, "position", title_end_position, 0.2)
+	tween.parallel().tween_property(back_button, "position", back_button_end_position, 0.2)
+	await get_tree().create_timer(0.2).timeout
+
+
+func _on_back_button_pressed():
+	await hide_screen()
+	main_menu_control.show_screen()
+
+
+func hide_screen():
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(title_label, "position", Vector2(0, -120), 0.2)
+	tween.parallel().tween_property(back_button, "position", Vector2(70, 1121), 0.2)
+	await get_tree().create_timer(0.2).timeout
---
+++ project/scripts/Main Menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+
+@export var button_container : VBoxContainer
+@export var title_label : Label
+
+@export_category("Pages")
+@export var game_development_control : Control
+
+
+func _on_game_development_pressed():
+	await hide_main_menu()
+	game_development_control.show_screen()
+
+
+func _on_game_modding_pressed():
+	hide_main_menu()
+
+
+func _on_artstation_pressed():
+	OS.shell_open("https://www.artstation.com/benjaminsaddler")
+
+
+func _on_other_pressed():
+	hide_main_menu()
+
+
+func hide_main_menu():
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(button_container, "position", Vector2(765.5, 1200), 0.2)
+	tween.parallel().tween_property(title_label, "position", Vector2(0, -120), 0.2)
+	await get_tree().create_timer(0.2).timeout
+
+
+func show_screen():
+	title_label.text = "Portfolio"
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(button_container, "position", Vector2(765.5, 408), 0.2)
+	tween.parallel().tween_property(title_label, "position", Vector2(0, 0), 0.2)
+	await get_tree().create_timer(0.2).timeout
